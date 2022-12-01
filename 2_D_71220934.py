c87e72668e19d1db9ba07a61729e5940f1acd535
yohanesthathit <117900755+yohanesthathit@users.noreply.github.com>
2022-12-01 17:15:15 +0700

    Add files via upload

---
+++ 2_D_71220934.py
@@ -0,0 +1,8 @@
+def cetakHuruf():
+    A = input("Masukkan kata: ")
+    if(A == "Valorant"):
+        print("Huruf paling ujung pada kata Valorant adalah ant")
+    else:
+        print("Huruf paling ujung pada kata Jerapah adalah Jer")
+    return(A)
+cetakHuruf()    
